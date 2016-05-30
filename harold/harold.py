fa8aa2a7c759a87baf50d4d162c93cae2b3a567e
Ilhan Polat <ilhanpolat@gmail.com>
2016-05-30 15:29:09 +0200

    Finally MIMO freqresp!!
    and lots of small bugfixes. Switching to test writing

--- harold/harold.py
+++ harold/harold.py
@@ -1328,12 +1328,18 @@ class Transfer:
         if isinstance(den,list):
             # Check the max number of elements in each entry
             max_deg_of_den = max([x.size for x in sum(den,[])])
-
+            if verbose:
+                print('In the MIMO context and proper entries, I\'ve '
+                      'found\nscalar denominator entries hence flagging '
+                      'as a static gain.')
             # If less than two, then den is a gain matrix.
             Gain_flag = True if max_deg_of_den == 1 else False
         else:
             Gain_flag = True if den.size == 1 else False
-            
+            if verbose:
+                print('In the SISO context and a proper rational function' 
+                      ', I\'ve found\na scalar denominator hence ' 
+                      'flagging as a static gain.')
 
         return num , den , shape , Gain_flag
 
@@ -2093,9 +2099,17 @@ class State:
         # If State() has a single nonzero argument then this is a gain
         # so flip the list and make d nonzero let the rest empty matrix. 
         if all(None_flags[1:]):
-            if verbose: print('I decided that this is a gain')
+            if verbose: print('Only A matrix is given in the'
+                              ' A,B,C,D arguments. Hence I decided'
+                              ' that this is a static gain')
             returned_abcd_list = list(reversed(returned_abcd_list))
             Gain_flag = True
+
+        # Or the nonzero argument is given (None,None,None,D) format
+        # hence pass with no modification
+        elif all(None_flags[:-1]):
+            if verbose: print('I decided that this is a gain')
+            Gain_flag = True            
 
         [a , b , c , d] = returned_abcd_list
         
@@ -2354,11 +2368,18 @@ def transfertostate(*tf_or_numden,output='system'):
     # Check if it is just a gain
     if it_is_gain:
         empty_size = max(m,p)
-        A = np.empty((empty_size,empty_size))
-        B = np.empty((empty_size,m))
-        C = np.empty((p,empty_size))
-        D = num/den
-        return A,B,C,D
+        A = np.array([],dtype=float)
+        B = np.array([],dtype=float)
+        C = np.array([],dtype=float)
+        if np.max((m,p)) > 1:
+            D = np.empty((m,p),dtype=float)
+            for rows in range(p):
+                for cols in range(m):
+                    D[rows,cols] = num[rows][cols]/den[rows][cols]
+        else:
+            D = num/den
+
+        return None,None,None,D
 
     if (m,p) == (1,1): # SISO
         A = haroldcompanion(den)
@@ -3580,7 +3601,7 @@ def staircase(A,B,C,compute_T=False,form='c',
             if block_indices:
                 return A0 , B0 , C0 , cble_block_indices
             else:
-                A0 , B0 , C0
+                return A0 , B0 , C0
 
     else: # Square system B full rank ==> trivially controllable
         cble_block_indices = np.array([n])
@@ -3714,8 +3735,9 @@ def minimal_realization(A,B,C,mu_tol=1e-9):
             keep_looking= False
         else:
             
-            Ac,Bc,Cc,blocks_c = staircase(A,B,C)
-            Ao,Bo,Co,blocks_o = staircase(A,B,C,form='o',invert=True)
+            Ac,Bc,Cc,blocks_c = staircase(A,B,C,block_indices=True)
+            Ao,Bo,Co,blocks_o = staircase(A,B,C,form='o',invert=True,
+                                              block_indices=True)
 
             # ===============Extra Check============================
             """            
@@ -4777,42 +4799,231 @@ def haroldpolydiv(dividend,divisor):
 
 # %% Frequency Domain
 
+#def frequency_response(G,custom_grid=None,high=None,low=None,samples=None,
+#                       custom_logspace=None,
+#                       input_freq_unit='Hz',output_freq_unit='Hz'):
+#    """
+#    Computes the frequency response matrix of a State() or Transfer()
+#    object. Transfer matrices are converted to state representations
+#    before the computations. The system representations are always 
+#    checked for minimality and, if any, unobservable/uncontrollable 
+#    modes are removed.
+#
+#    """
+#    if not isinstance(G,(State,Transfer)):
+#        raise ValueError('The argument should either be a State() or '
+#                         'Transfer() object. I have found {0}'
+#                         ''.format(type(G).__qualname__))
+#    
+#
+#    for x in (input_freq_unit,output_freq_unit):
+#        if x not in ('Hz','rad/s'):
+#            raise ValueError('I can only handle "Hz" and "rad/s" as '
+#                             'frequency units. "{0}" is not recognized.'
+#                             ''.format(x))
+#
+#    """ If the system has very small or zero poles/zeros then 
+#     the list should be shifted to meaningful region because the result
+#     would either be huge or unpractically small especially in logaritmic
+#     scale, completely useless."""     
+#
+#    if G._isgain:
+#        samples = 2
+#        high = -2
+#        low = 2
+#    else:
+#        pz_list = np.append(G.poles,G.zeros)
+#
+#        if G.SamplingSet == 'Z':
+#            nat_freq = np.abs(np.log(pz_list / G.SamplingPeriod))
+#        else:
+#            nat_freq = np.abs(pz_list)
+#
+#        smallest_pz = np.max([np.min(nat_freq),1e-7])
+#        largest_pz  = np.max([np.max(nat_freq),smallest_pz+10])
+#
+#    # The order of hierarchy is as follows:
+#    #  - We first check if a custom frequency grid is supplied
+#    #  - If None, then we check if a logspace-like option is given
+#    #  - If that's also None we check whether custom logspace
+#    #       limits are supplied with defaults for missing
+#    #           .. high    --> +2 decade from the fastest pole/zero
+#    #           .. low     --> -3 decade from the slowest pole/zero
+#    #           .. samples --> 1000 points
+#
+#    # TODO: Implement a better/nonuniform algo for discovering new points 
+#    # around  poles and zeros. Right now there is a chance to hit a pole 
+#    # or a zero head on. 
+#    # matlab coarseness stuff is nice but in practice leads to weirdness
+#    # even when granularity = 4.
+#
+#    if custom_grid is None:
+#        if custom_logspace is None:
+#            high = np.ceil(np.log10(largest_pz)) + 1 if high is None else high
+#            low  = np.floor(np.log10(smallest_pz)) - 1 if low  is None else low
+#            samples = 1000 if samples is None else samples
+#        else:
+#            high , low , samples = custom_logspace
+#        w = np.logspace(low,high,samples)
+#    else:
+#        w = np.asarray(custom_grid,dtype='float')
+#
+#    # Convert to Hz if necessary
+#    if not input_freq_unit == 'Hz':
+#        w = np.rad2deg(w)
+#
+#    iw = 1j*w.flatten()
+#
+#    # TODO: This has to be rewritten, currently extremely naive
+#    if G._isSISO:
+#        freq_resp_array = np.empty_like(iw,dtype='complex')
+#        
+#        if isinstance(G,State):
+#            Gtf = statetotransfer(G)
+#        else:
+#            Gtf = G
+#        freq_resp_array = (np.polyval(Gtf.num[0],iw) /
+#                           np.polyval(Gtf.den[0],iw)
+#                           )
+#    else:
+#        p , m = G.shape
+#        freq_resp_array = np.empty((p,m,len(iw)),dtype='complex')
+#        if isinstance(G,State):
+#            Gtf = statetotransfer(G)
+#        else:
+#            Gtf = G
+#        for rows in range(p):
+#            for cols in range(m):
+#                freq_resp_array[rows,cols,:] = (
+#                        np.polyval(Gtf.num[rows][cols][0],iw) /
+#                        np.polyval(Gtf.den[rows][cols][0],iw)
+#                        )
+#
+#    return freq_resp_array , w
+#
+
+def _State_frequency_response_generator(mA,mb,sc,f):
+    """
+    This is the low level function to generate the frequency response
+    values for a state space representation. The realization must be
+    strictly in the observable Hessenberg form.
+
+    Implements the inner loop of Misra, Patel SIMAX 1988 Algo. 3.1 in 
+    batches of B matrices instead of looping over every column of B.
+
+    Parameters
+    ----------
+   
+    mA : array_like {n x n}
+        The A matrix of the realization in the upper Hessenberg form
+    mb : array_like {n x m}
+        The B vector of the realization
+    sc : float
+        The only nonzero coefficient of the o'ble-Hessenberg form
+    f  : array_like
+        The frequency grid
+
+    Returns
+    -------
+    r  : complex-valued numpy array
+
+    """
+   
+    nn , m = mA.shape[0] , mb.shape[1]
+    r  = np.empty((f.size, m),dtype=complex)
+    Ab = np.hstack((-mA,mb)).astype(complex)
+    X = np.empty_like(Ab)
+
+    imag_indices = np.diag_indices(nn)
+   
+    for ind , val in enumerate(f):
+        X[:,:] = Ab # Working copy
+        X[imag_indices] += val*1j
+        for x in range(1,nn):
+            X[x] -= (X[x,x-1] / X[x-1,x-1]) * X[x-1]
+            
+        r[ind,:] = X[-1,-m:]/X[-1,-1-m]
+
+    return r*sc
+
+
+   
+
 def frequency_response(G,custom_grid=None,high=None,low=None,samples=None,
                        custom_logspace=None,
                        input_freq_unit='Hz',output_freq_unit='Hz'):
     """
     Computes the frequency response matrix of a State() or Transfer()
-    object. Transfer matrices are converted to state representations
-    before the computations. The system representations are always 
-    checked for minimality and, if any, unobservable/uncontrollable 
-    modes are removed.
+    object. 
+    
+    The State representations are always checked for minimality and, 
+    if any, unobservable/uncontrollable modes are removed.
+    
+    Parameters
+    ----------
+    G: State of Transfer
+        The realization for which the frequency response is computed
+    custom_grid : array_like
+        An array of sorted positive numbers denoting the frequencies
+    high : float
+        Power of 10 denoting the maximum frequency. If a discrete 
+        realization is given this is overridden by the Nyquist frequency. 
+    low : float
+        Power of 10 denoting the minimum frequency.
+    samples: int
+        Number of samples to be created between `high` and `low`
+    custom_logspace: 3-tuple
+    
+    
+    Returns
+    -------
+    freq_resp_array : Complex_valued numpy array
+        The frequency response of the system G
+    w : 1D numpy array
+        Frequency grid that is used to evaluate the frequency response
+        
+    
 
     """
+    ############################################################
+    # TODO: Investigate if *args,*kwargs is a better syntax here for 
+    # better argument parsing. 
+    ############################################################    
+    
     if not isinstance(G,(State,Transfer)):
         raise ValueError('The argument should either be a State() or '
-                         'Transfer() object. I have found {0}'
+                         'Transfer() object. I have found a {0}'
                          ''.format(type(G).__qualname__))
-    
+   
 
     for x in (input_freq_unit,output_freq_unit):
         if x not in ('Hz','rad/s'):
             raise ValueError('I can only handle "Hz" and "rad/s" as '
                              'frequency units. "{0}" is not recognized.'
                              ''.format(x))
+   
 
-    """ If the system has very small or zero poles/zeros then 
-     the list should be shifted to meaningful region because the result
-     would either be huge or unpractically small especially in logaritmic
-     scale, completely useless."""     
+    _is_discrete = G.SamplingSet == 'Z'
+    
+    if _is_discrete:
+        nyq_freq = 1/(2*G.SamplingPeriod)
+
+    # We first check whether we need to bother if G is a gain
+    # which overrides the user input for the grid in Hz. except 
+    # the output_freq_unit
 
     if G._isgain:
         samples = 2
-        high = -2
-        low = 2
+        if _is_discrete:
+            high = nyq_freq
+            low = np.floor(np.log10(nyq_freq))
+        else:
+            high = 2
+            low = -2
     else:
         pz_list = np.append(G.poles,G.zeros)
 
-        if G.SamplingSet == 'Z':
+        if _is_discrete:
             nat_freq = np.abs(np.log(pz_list / G.SamplingPeriod))
         else:
             nat_freq = np.abs(pz_list)
@@ -4829,13 +5040,14 @@ def frequency_response(G,custom_grid=None,high=None,low=None,samples=None,
     #           .. low     --> -3 decade from the slowest pole/zero
     #           .. samples --> 1000 points
 
-    # TODO: Implement a better/nonuniform algo for discovering new points 
-    # around  poles and zeros. Right now there is a chance to hit a pole 
-    # or a zero head on. 
-    # matlab coarseness stuff is nice but in practice leads to weirdness
-    # even when granularity = 4.
+    # TODO: Implement a better/nonuniform algo for discovering new points
+    # around  poles and zeros. Right now there is a chance to hit a pole
+    # or a zero head on. matlab coarseness in practice leads to some 
+    # weirdness even when granularity = 1.
 
-    if custom_grid is None:
+    if G._isgain:
+        w = np.logspace(low,high,samples)
+    elif custom_grid is None:
         if custom_logspace is None:
             high = np.ceil(np.log10(largest_pz)) + 1 if high is None else high
             low  = np.floor(np.log10(smallest_pz)) - 1 if low  is None else low
@@ -4848,39 +5060,81 @@ def frequency_response(G,custom_grid=None,high=None,low=None,samples=None,
 
     # Convert to Hz if necessary
     if not input_freq_unit == 'Hz':
-        w = np.rad2deg(w)
-
-    iw = 1j*w.flatten()
-
-    # TODO: This has to be rewritten, currently extremely naive
-    if G._isSISO:
-        freq_resp_array = np.empty_like(iw,dtype='complex')
-        
-        if isinstance(G,State):
-            Gtf = statetotransfer(G)
+        w = np.rad2deg(w)   
+    
+    if G._isgain:
+        if G._isSISO:
+            if isinstance(G,Transfer):
+                freq_resp_array = np.array([1]*2)*G.num[0,0]
+            else:
+                freq_resp_array = np.array([1]*2)*G.d[0,0]
         else:
-            Gtf = G
-        freq_resp_array = (np.polyval(Gtf.num[0],iw) /
-                           np.polyval(Gtf.den[0],iw)
-                           )
+            if isinstance(G,Transfer):
+                freq_resp_array = np.zeros((2,)+G.shape) + np.array(G.num)
+            else:
+                freq_resp_array = np.zeros((2,)+G.shape) + np.array(G.d)
+            
+            
+            freq_resp_array = np.rollaxis(freq_resp_array,0,3)
+
+    elif G._isSISO:
+        freq_resp_array = np.zeros_like(w,dtype='complex')
+       
+        if isinstance(G,State):
+            aa , bb , cc = staircase(
+                            *minimal_realization(*G.matrices[:-1]),
+                            form='o',
+                            invert=True
+                            )
+            freq_resp_array = _State_frequency_response_generator(
+                                                        aa,bb,cc[0,-1],w)
+
+            if np.any(G.d):
+                freq_resp_array += G.d[0,0]
+
+        else:
+            iw = w.flatten()*1j
+            freq_resp_array = (np.polyval(G.num[0],iw) /
+                               np.polyval(G.den[0],iw)
+                               )
     else:
         p , m = G.shape
-        freq_resp_array = np.empty((p,m,len(iw)),dtype='complex')
+        freq_resp_array = np.empty((len(w),m,p),dtype='complex')
+        
         if isinstance(G,State):
-            Gtf = statetotransfer(G)
+            aa , bb , cc = minimal_realization(*G.matrices[:-1])
+            
+            for rows in range(p):
+                aaa , bbb , ccc = staircase(aa,bb,cc[[rows],:],form='o',invert=True)
+                freq_resp_array[:,:,rows] = _State_frequency_response_generator(
+                                                            aaa,bbb,ccc[0,-1],w)
+
+
+            if np.any(G.d):
+                freq_resp_array += G.d[0,0].T
+
+            # Currently the shape is (freqs,cols,rows) for broadcasting.
+            # roll axes in the correct places to have (row, col, freq) shape
+            freq_resp_array = np.rollaxis(np.rollaxis(freq_resp_array,0,3),1)
+            
+            
         else:
-            Gtf = G
-        for rows in range(p):
-            for cols in range(m):
-                freq_resp_array[rows,cols,:] = (
-                        np.polyval(Gtf.num[rows][cols][0],iw) /
-                        np.polyval(Gtf.den[rows][cols][0],iw)
-                        )
+            iw = w.flatten()*1j
+            freq_resp_array = np.empty((len(w),m,p),dtype='complex')
+            for rows in range(p):
+                for cols in range(m):
+                    freq_resp_array[:,cols,rows] = (
+                            np.polyval(G.num[rows][cols].flatten(),iw) /
+                            np.polyval(G.den[rows][cols].flatten(),iw)
+                            )
+            freq_resp_array = np.rollaxis(np.rollaxis(freq_resp_array,0,3),1)
+
 
     return freq_resp_array , w
-
-
-
+    
+    
+    
+    
 def pair_complex_numbers(somearray , tol = 1e-9 , realness_tol=1e-9 , 
                          positives_first=False , reals_first = True):
     """
@@ -5043,3 +5297,53 @@ def pair_complex_numbers(somearray , tol = 1e-9 , realness_tol=1e-9 ,
             return np.r_[array_r_j[~imagness] , paired_cmplx_part]
         else:
             return np.r_[paired_cmplx_part , array_r_j[~imagness]]
+            
+            
+def matrix_printer(M , num_format="f" , var_name=None):
+    """
+    This is a simple helper function for quickly carrying matrix data 
+    to matlab. The array is assumed to be 1- or 2D. If the array is 3D
+    or more then use a slice in a for loop with explicit naming since 
+    matlab does not have multidimensional entry syntax. 
+    
+    The `var_name` can be used in a loop for such purposes e.g.:: 
+    
+        matrix_printer(M,var_name = 'A(:,:,2)')
+    
+    which will include the string `A(:,:,2) =` at the beginning of the 
+    output string. 
+    
+    This function is inspired by 
+    [Lee Archer's gist](https://gist.github.com/lbn/836313e283f5d47d2e4e)
+    
+    Parameters
+    ----------
+    M : Numpy array
+        The numpy array that is going to be pretty printed in matlab format.
+    num_format : str
+        String specifier (See Python documentation Section 6.1)
+    var_name : str
+        The name of the variable to be created in matlab. 
+
+    Returns
+    -------
+    mat : str
+        Resulting string
+    
+    """
+    
+    # For every row, get the lengths of the entries in the current string
+    # format and max over them.
+
+    M = np.atleast_2d(M)
+    m = M.shape[0]
+    col_maxes = [max([len(("{:"+num_format+"}").format(x)) for x in col]) for col in M.T]
+    start_str = var_name + ' = [' if var_name is not None else '['
+    mat = start_str
+    
+    for rowind , x in enumerate(M):
+        for colind , y in enumerate(x):
+            mat += (" {: ."+str(col_maxes[colind])+num_format+"}").format(y) + "  "
+        mat += "];\n" if m == rowind + 1 else ";\n"
+
+    return mat
