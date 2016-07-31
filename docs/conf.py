c0e302c72862f919667b759ec157ee01bd0f37dd
Ilhan Polat <ilhanpolat@gmail.com>
2016-07-31 23:53:26 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 import shlex
 import mock
 
-MOCK_MODULES = ['tabulate']
+MOCK_MODULES = ['tabulate','scipy.signal']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
