51a4d3ec51db823dfa35f175343dd0fe17b65993
Ilhan Polat <ilhanpolat@gmail.com>
2016-07-31 23:40:38 +0200

    import this from that --> break everything ever

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 import shlex
 import mock
 
-MOCK_MODULES = ['tabulate', 'scipy', 'numpy']
+MOCK_MODULES = ['tabulate', 'scipy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
