MAINT:   fix wrong order of func calls

harold/_polynomial_ops.py
@@ -250,7 +250,7 @@ def haroldgcd(*args):
                  over this.
 
     """
-    raw_arr_args = [np.squeeze(np.atleast_1d(x)) for x in args]
+    raw_arr_args = [np.atleast_1d(np.squeeze(x)) for x in args]
     arr_args = [np.trim_zeros(x, 'f') for x in raw_arr_args if x.size > 0]
     dimension_list = [x.ndim for x in arr_args]
 