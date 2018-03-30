f24735bd883745a25b50bba49c830f978c62197f
Ilhan Polat <ilhanpolat@gmail.com>
2018-03-30 16:47:03 +0200

    TST: Test at least bode, nyquist plot shapes

--- harold/tests/test_frequency_domain_plots.py
+++ harold/tests/test_frequency_domain_plots.py
@@ -1,26 +1,29 @@
-from harold import State, Transfer, step_response_plot, impulse_response_plot
+from harold import State, Transfer, bode_plot, nyquist_plot
 
 from numpy import eye
 from numpy.random import rand, seed
 from numpy.testing import assert_equal
 
 
-def test_step_response_plot_shape():
+def test_bode_plot_shape():
     seed(1234)
-    assert_equal(len(step_response_plot(Transfer(5, dt=0.5)).axes), 1)
+    # SISO
+    assert_equal(len(bode_plot(Transfer(5, dt=0.5)).axes), 2)
+    # MIMO
     a, b, c = -3*eye(5) + rand(5, 5), rand(5, 3), rand(4, 5)
     G = State(a, b, c)
-    f = step_response_plot(G)
-    # Ridiculous API
+    f = bode_plot(G)
     prop = f.axes[0].get_subplotspec().get_topmost_subplotspec().get_gridspec()
-    assert_equal(prop.get_geometry(), G.shape)
+    assert_equal(prop.get_geometry(), (G.shape[0]*2, G.shape[1]))
 
 
-def test_impulse_response_plot_shape():
+def test_nyquist_plot_shape():
     seed(1234)
-    assert_equal(len(impulse_response_plot(Transfer(5, dt=0.5)).axes), 1)
+    # SISO
+    assert_equal(len(nyquist_plot(Transfer(eye(2), dt=0.5)).axes), 4)
+    # MIMO
     a, b, c = -3*eye(5) + rand(5, 5), rand(5, 3), rand(4, 5)
     G = State(a, b, c)
-    f = impulse_response_plot(G)
+    f = nyquist_plot(G)
     prop = f.axes[0].get_subplotspec().get_topmost_subplotspec().get_gridspec()
     assert_equal(prop.get_geometry(), G.shape)
