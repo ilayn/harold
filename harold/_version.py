e1ed41cc15eaece73c0701e5774a62f9f4f039a6
ilayn <ilhanpolat@gmail.com>
2022-04-13 15:46:58 +0200

    CI: Switch to poetry

---
+++ harold/_version.py
@@ -0,0 +1,7 @@
+__title__ = 'harold'
+__description__ = 'An open-source systems and controls toolbox for Python3.'
+__version__ = '1.0.2'
+__author__ = 'Ilhan Polat'
+__author_email__ = 'harold.of.python@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Ilhan Polat'
